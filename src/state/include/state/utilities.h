fbbbbb37522c61c08fb4a404f6da732dd7cf27f8
Sai Hemanth B <saihemanth9019@gmail.com>
2018-10-02 18:05:45 +0530

    Add i_command_taker and i_updatable interfaces

---
+++ src/state/include/state/utilities.h
@@ -0,0 +1,33 @@
+/**
+ * @file utilities.h
+ * Set of types and utilities functions for state
+ */
+
+#ifndef STATE_UTILITIES_H
+#define STATE_UTILITIES_H
+
+#include <cstdint>
+
+namespace state {
+
+/**
+ * Integer type to represent unique Actor IDs
+ */
+typedef int64_t ActorId;
+
+/**
+ * Enum for the different actor unit types
+ */
+enum class ActorType { SOLDIER, VILLAGER, FACTORY_SOLDIER, FACTORY_VILLAGER };
+
+/**
+ * Enum for the two game players
+ */
+enum class PlayerId : int {
+	PLAYER1 = 0,
+	PLAYER2 = 1,
+	PLAYER_COUNT = 2,
+};
+} // namespace state
+
+#endif
---
+++ src/state/src/state.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file state.cpp
+ * Definitions for functions of the State class
+ */
+
+#include "state/state.h"
+
+namespace state {}
