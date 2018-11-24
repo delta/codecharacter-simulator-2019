1d78a244d207193bff7fa5300e7a0367a5e9553c
venkat24 <venkatramansrikanth@outlook.com>
2018-11-24 22:23:33 +0530

    Add constants module and some constants for State

---
+++ src/constants/include/constants/state.h
@@ -0,0 +1,23 @@
+/**
+ * @file state.h
+ * Constants related to game state
+ */
+
+#ifndef CONSTANTS_INCLUDE_CONSTANTS_STATE_H
+#define CONSTANTS_INCLUDE_CONSTANTS_STATE_H
+
+#include <cstddef>
+
+// Maximum number of soldiers per player
+const size_t MAX_NUM_SOLDIERS = 100;
+
+// Maximum number of villagers per player
+const size_t MAX_NUM_VILLAGERS = 100;
+
+// Maximum number of factories per player
+const size_t MAX_NUM_FACTORIES = 50;
+
+// Size of the map in grid elements
+const size_t MAP_SIZE = 50;
+
+#endif
