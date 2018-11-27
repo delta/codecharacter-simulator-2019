ed41623480a39b0be546e88e343e4929d867bac6
hrishikeshshekhar <hrishikeshshekhar@gmail.com>
2018-11-27 18:12:53 +0530

    Clang fomrat done

--- src/constants/include/constants/gold_manager.h
+++ src/constants/include/constants/gold_manager.h
@@ -8,25 +8,25 @@
 
 #include <cstddef>
 
-//Amount of gold each player has at the start of the game
+// Amount of gold each player has at the start of the game
 const int64_t GOLD_START = 300;
 
 // Maxiumm amount of gold each player can have
 const int64_t GOLD_MAX = INT64_MAX;
 
-//Reward for killing one soldier
+// Reward for killing one soldier
 const int64_t SOLDIER_KILL_REWARD_AMOUNT = 200;
 
-//Reward for killing one villager
+// Reward for killing one villager
 const int64_t VILLAGER_KILL_REWARD_AMOUNT = 100;
 
-//Amount of gold to create soldier
+// Amount of gold to create soldier
 const int64_t SOLDIER_COST = 200;
 
-//Amount of gold to create soldier
+// Amount of gold to create soldier
 const int64_t VILLAGER_COST = 200;
 
-//Amount of gold to create soldier
+// Amount of gold to create soldier
 const int64_t FACTORY_COST = 500;
 
 #endif
--- src/state/src/gold_manager/gold_manager.cpp
+++ src/state/src/gold_manager/gold_manager.cpp
@@ -7,132 +7,123 @@
 #include "physics/vector.hpp"
 #include <stdexcept>
 
-namespace state{
+namespace state {
 
-	GoldManager::GoldManager(){
+GoldManager::GoldManager() {}
 
+GoldManager::GoldManager(std::vector<int64_t> players_gold, int64_t max_gold,
+                         int64_t soldier_kill_reward_amount,
+                         int64_t villager_kill_reward_amount,
+                         int64_t villager_cost, int64_t soldier_cost,
+                         int64_t factory_cost)
+    : players_gold(players_gold), max_gold(max_gold),
+      soldier_kill_reward_amount(soldier_kill_reward_amount),
+      villager_kill_reward_amount(villager_kill_reward_amount),
+      villager_cost(villager_cost), soldier_cost(soldier_cost),
+      factory_cost(factory_cost) {}
+
+void GoldManager::Increase(PlayerId player_id, int64_t amount) {
+
+	int64_t current_player_id = static_cast<int>(player_id);
+
+	if (amount <= 0) {
+
+		throw std::out_of_range("Increase in gold amount should be positive");
 	}
 
-	GoldManager::GoldManager(std::vector<int64_t> players_gold, int64_t max_gold,
-		                     int64_t soldier_kill_reward_amount, int64_t villager_kill_reward_amount,
-		                     int64_t villager_cost, int64_t soldier_cost, 
-		                     int64_t factory_cost)
-							:players_gold(players_gold), max_gold(max_gold),
-							 soldier_kill_reward_amount(soldier_kill_reward_amount),
-							 villager_kill_reward_amount(villager_kill_reward_amount),
-							 villager_cost(villager_cost), soldier_cost(soldier_cost),
-							 factory_cost(factory_cost) {}
+	players_gold[current_player_id] += amount;
 
-	void GoldManager::Increase(PlayerId player_id, int64_t amount){
+	if (players_gold[current_player_id] > max_gold) {
 
-		int64_t current_player_id = static_cast<int>(player_id);
+		players_gold[current_player_id] = max_gold;
+	}
+}
 
-		if (amount <= 0) {
+void GoldManager::Decrease(PlayerId player_id, int64_t amount) {
 
-			throw std::out_of_range("Increase in gold amount should be positive");
-		}
+	int64_t current_player_id = static_cast<int>(player_id);
 
-		players_gold[current_player_id] += amount;
+	if (amount <= 0) {
 
-		if(players_gold[current_player_id] > max_gold){
-
-			players_gold[current_player_id] = max_gold;
-		}
+		throw std::out_of_range(
+		    "The amount of descrese in gold must be positive");
 	}
 
-	void GoldManager::Decrease(PlayerId player_id, int64_t amount){
+	if (amount >= players_gold[current_player_id]) {
 
-		int64_t current_player_id = static_cast<int>(player_id);
-
-		if(amount <= 0){
-
-			throw std::out_of_range("The amount of descrese in gold must be positive");
-		}
-
-		if(amount >= players_gold[current_player_id]){
-
-			throw std::out_of_range("The decrease in gold is greater than current balance");
-		}
-
-		players_gold[current_player_id] -= amount;
+		throw std::out_of_range(
+		    "The decrease in gold is greater than current balance");
 	}
 
-	void GoldManager::RewardKill(Actor *enemy_actor){
-		
-		//Finding player ID of player to increment gold
-		PlayerId player_id;
-		
-		PlayerId enemy_player_id = enemy_actor->GetPlayerId();
+	players_gold[current_player_id] -= amount;
+}
 
-		if(enemy_player_id == PlayerId::PLAYER2){
-			player_id = PlayerId::PLAYER1;
-		}
-		else{
-			player_id = PlayerId::PLAYER2;
-		}
+void GoldManager::RewardKill(Actor *enemy_actor) {
 
-		ActorType enemy_actor_type = enemy_actor->GetActorType();
-		int64_t reward_amount;
+	// Finding player ID of player to increment gold
+	PlayerId player_id;
 
-		if(enemy_actor_type == ActorType::SOLDIER){
-			reward_amount = soldier_kill_reward_amount;
-		}
+	PlayerId enemy_player_id = enemy_actor->GetPlayerId();
 
-		else{
-			reward_amount = villager_kill_reward_amount;
-		}
-
-		Increase(player_id, reward_amount);
+	if (enemy_player_id == PlayerId::PLAYER2) {
+		player_id = PlayerId::PLAYER1;
+	} else {
+		player_id = PlayerId::PLAYER2;
 	}
 
-	int64_t GoldManager::CreateUnitCost(Actor *actor){
+	ActorType enemy_actor_type = enemy_actor->GetActorType();
+	int64_t reward_amount;
 
-		ActorType unit_type = actor->GetActorType();
-
-		if(unit_type == ActorType::FACTORY_SOLDIER || unit_type == ActorType::FACTORY_VILLAGER)
-		{
-			return factory_cost;
-		}
-
-		else if(unit_type == ActorType::VILLAGER)
-		{
-			return villager_cost;
-		}
-
-		else
-		{
-			return soldier_cost;
-		}
+	if (enemy_actor_type == ActorType::SOLDIER) {
+		reward_amount = soldier_kill_reward_amount;
 	}
 
-	void GoldManager::CreateCost(PlayerId player_id, Actor *actor){
-
-		int64_t actor_cost = CreateUnitCost(actor);
-		int64_t current_balance = GetBalance(player_id);
-
-		//Decreasing player's gold if they can buy the unit
-		if(current_balance < actor_cost){
-
-			throw std::out_of_range("Do not possess enough gold to buy unit");
-		}
-		
-		else{
-
-			Decrease(player_id, actor_cost);
-
-		}
-
+	else {
+		reward_amount = villager_kill_reward_amount;
 	}
 
-	int64_t GoldManager::GetBalance(PlayerId player_id){
+	Increase(player_id, reward_amount);
+}
 
-		return players_gold[static_cast<int>(player_id)];
+int64_t GoldManager::CreateUnitCost(Actor *actor) {
 
+	ActorType unit_type = actor->GetActorType();
+
+	if (unit_type == ActorType::FACTORY_SOLDIER ||
+	    unit_type == ActorType::FACTORY_VILLAGER) {
+		return factory_cost;
 	}
 
-	int64_t GoldManager::GetMaxGold(){
-
-		return max_gold;
-		
+	else if (unit_type == ActorType::VILLAGER) {
+		return villager_cost;
 	}
+
+	else {
+		return soldier_cost;
+	}
+}
+
+void GoldManager::CreateCost(PlayerId player_id, Actor *actor) {
+
+	int64_t actor_cost = CreateUnitCost(actor);
+	int64_t current_balance = GetBalance(player_id);
+
+	// Decreasing player's gold if they can buy the unit
+	if (current_balance < actor_cost) {
+
+		throw std::out_of_range("Do not possess enough gold to buy unit");
+	}
+
+	else {
+
+		Decrease(player_id, actor_cost);
+	}
+}
+
+int64_t GoldManager::GetBalance(PlayerId player_id) {
+
+	return players_gold[static_cast<int>(player_id)];
+}
+
+int64_t GoldManager::GetMaxGold() { return max_gold; }
 }
