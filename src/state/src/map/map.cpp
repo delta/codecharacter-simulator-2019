43f262f6d1800e530e058fd6fcec1c400b34c379
TanviKumar <tanvikumar777@gmail.com>
2018-12-13 01:45:59 +0530

    Mark map methods as const

--- src/state/src/map/map.cpp
+++ src/state/src/map/map.cpp
@@ -13,14 +13,14 @@ Map::Map(std::vector<std::vector<TerrainType>> map, size_t map_size,
          size_t element_size)
     : map(map), map_size(map_size), element_size(element_size) {}
 
-size_t Map::GetSize() { return map_size; }
-size_t Map::GetElementSize() { return element_size; }
+size_t Map::GetSize() const { return map_size; }
+size_t Map::GetElementSize() const { return element_size; }
 
-TerrainType Map::GetTerrainTypeByOffset(int64_t x, int64_t y) {
+TerrainType Map::GetTerrainTypeByOffset(int64_t x, int64_t y) const {
 	return map[x][y];
 }
 
-TerrainType Map::GetTerrainTypeByPosition(int64_t x, int64_t y) {
+TerrainType Map::GetTerrainTypeByPosition(int64_t x, int64_t y) const {
 	auto offset_x = x / element_size;
 	auto offset_y = y / element_size;
 
