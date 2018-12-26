56c54653677b059934213a87891a19f3acc3754f
TanviKumar <tanvikumar777@gmail.com>
2018-12-26 12:44:08 +0530

    Create mocks for logger and state

---
+++ test/logger/mocks/logger_mock.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "gmock/gmock.h"
+#include "logger/error_type.h"
+#include "logger/interfaces/i_logger.h"
+#include "state/interfaces/i_command_taker.h"
+#include "state/utilities.h"
+
+#include <cstdint>
+#include <ostream>
+#include <string>
+
+using namespace std;
+using namespace logger;
+using namespace state;
+
+class LoggerMock : public ILogger {
+  public:
+	MOCK_METHOD1(LogState, void(ICommandTaker *));
+	MOCK_METHOD2(LogInstructionCount, void(PlayerId, int64_t));
+	MOCK_METHOD3(LogError, void(PlayerId, ErrorType, string));
+	MOCK_METHOD0(LogFinalGameParams, void());
+	MOCK_METHOD1(WriteGame, void(std::ostream &));
+};
