cbff9633d2e4901c454d010bfabe92874204edc2
TanviKumar <tanvikumar777@gmail.com>
2018-12-26 12:39:22 +0530

    Add inst limits and max HP constants

--- src/constants/include/constants/state.h
+++ src/constants/include/constants/state.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
 // Maximum number of soldiers per player
 const size_t MAX_NUM_SOLDIERS = 100;
@@ -18,3 +19,12 @@ const size_t MAX_NUM_FACTORIES = 50;
 
 // Size of the map in grid elements
 const size_t MAP_SIZE = 50;
+
+// Max Starting HP for Soldier
+const int64_t SOLDIER_MAX_HP = 200;
+
+// Max Starting HP for Villager
+const int64_t VILLAGER_MAX_HP = 80;
+
+// Max Starting HP for Factory
+const int64_t FACTORY_MAX_HP = 3000;
