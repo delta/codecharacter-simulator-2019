5466c17d52afa479e0d7a19a6c8de47bce27801d
venkat24 <venkatramansrikanth@outlook.com>
2019-02-16 16:25:55 +0530

    Fix build_request.h
    - Add docstring
    - Fix clangformat issues

--- src/state/include/state/build_request.h
+++ src/state/include/state/build_request.h
@@ -1,7 +1,17 @@
+/**
+ * @file build_request.h
+ * Specifies struct that represents one request to build, by a villager
+ */
+
+#pragma once
+
 #include "state/utilities.h"
 
 namespace state {
 
+/**
+ * Represents one request to build, by a villager
+ */
 struct BuildRequest {
 	/**
 	 * Id of the villager who is trying to build the factory
@@ -11,7 +21,7 @@ struct BuildRequest {
 	/**
 	 * The type of actor that the player wants to factory to produce
 	 */
-	state::ActorType prod_type;
+	ActorType prod_type;
 
 	/**
 	 * A vector for the offset position where the factory is to be built
