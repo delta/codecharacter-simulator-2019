87bf1b83580bf0989862ea8636009fdadffe2934
venkat24 <venkatramansrikanth@outlook.com>
2019-02-25 19:43:47 +0530

    Tweak Constants

--- src/constants/include/constants/score_manager.h
+++ src/constants/include/constants/score_manager.h
@@ -11,16 +11,16 @@
 namespace score_constants {
 
 // Reward for killing a villager
-const int64_t VILLAGER_KILL_REWARD = 5;
+const int64_t VILLAGER_KILL_REWARD = 12;
 
 // Reward for killing a soldier
-const int64_t SOLDIER_KILL_REWARD = 9;
+const int64_t SOLDIER_KILL_REWARD = 25;
 
 // Reward for killing a factory
 const int64_t FACTORY_KILL_REWARD = 45;
 
 // Reward for constructing a factory
-const int64_t FACTORY_CONSTRUCTION_REWARD = 60;
+const int64_t FACTORY_CONSTRUCTION_REWARD = 100;
 
 // Age levels at which units receive rewards
 const auto UNIT_AGE_LEVELS = std::vector<int64_t>{50, 100, 200, 500};
@@ -29,7 +29,7 @@ const auto UNIT_AGE_LEVELS = std::vector<int64_t>{50, 100, 200, 500};
 const auto VILLAGER_AGE_REWARDS = std::vector<int64_t>{4, 9, 19, 55};
 
 // Rewards for incremental ages of a soldier
-const auto SOLDIER_AGE_REWARDS = std::vector<int64_t>{7, 14, 24, 64};
+const auto SOLDIER_AGE_REWARDS = std::vector<int64_t>{3, 7, 16, 43};
 
 // Age levels at which factories receive rewards
 const auto FACTORY_AGE_LEVELS = std::vector<int64_t>{100, 200, 400, 800};
@@ -38,6 +38,6 @@ const auto FACTORY_AGE_LEVELS = std::vector<int64_t>{100, 200, 400, 800};
 const auto FACTORY_AGE_REWARDS = std::vector<int64_t>{21, 42, 84, 168};
 
 // Fraction of final gold to reward the player as score
-const double GOLD_REWARD_RATIO = 0.01;
+const double GOLD_REWARD_RATIO = 0.02;
 
 } // namespace score_constants
