9dad19e3961ac615f21f5de677835971d36970b4
venkat24 <venkatramansrikanth@outlook.com>
2019-02-25 19:43:47 +0530

    Tweak Constants

--- src/constants/include/constants/gold_manager.h
+++ src/constants/include/constants/gold_manager.h
@@ -14,25 +14,25 @@ const int64_t GOLD_START = 300;
 const int64_t GOLD_MAX = INT64_MAX;
 
 // Reward for killing one soldier
-const int64_t SOLDIER_KILL_REWARD_AMOUNT = 200;
+const int64_t SOLDIER_KILL_REWARD_AMOUNT = 100;
 
 // Reward for killing one villager
-const int64_t VILLAGER_KILL_REWARD_AMOUNT = 100;
+const int64_t VILLAGER_KILL_REWARD_AMOUNT = 30;
 
 // Amount of gold to create soldier
-const int64_t SOLDIER_COST = 200;
+const int64_t SOLDIER_COST = 400;
 
-// Amount of gold to create soldier
-const int64_t VILLAGER_COST = 200;
+// Amount of gold to create villager
+const int64_t VILLAGER_COST = 350;
 
-// Amount of gold to create soldier
-const int64_t FACTORY_COST = 500;
+// Amount of gold to create factory
+const int64_t FACTORY_COST = 2000;
 
 // Amount of gold for destroying a factory
-const int64_t FACTORY_KILL_REWARD_AMOUNT = 600;
+const int64_t FACTORY_KILL_REWARD_AMOUNT = 400;
 
 // Amount of gold recieved for factory suiciding
 const int64_t FACTORY_SUICIDE_PENALTY = 200;
 
 // Amount of gold increase per villager mining
-const int64_t MINING_REWARD = 10;
+const int64_t MINING_REWARD = 20;
--- src/constants/include/constants/state.h
+++ src/constants/include/constants/state.h
@@ -11,16 +11,16 @@
 #include <vector>
 
 // Maximum number of soldiers per player
-const size_t MAX_NUM_SOLDIERS = 30;
+const size_t MAX_NUM_SOLDIERS = 40;
 
 // Maximum number of villagers per player
-const size_t MAX_NUM_VILLAGERS = 30;
+const size_t MAX_NUM_VILLAGERS = 80;
 
 // Maximum number of factories per player
-const size_t MAX_NUM_FACTORIES = 20;
+const size_t MAX_NUM_FACTORIES = 30;
 
 // Number of villagers that each player starts with
-const size_t NUM_VILLAGERS_START = 10;
+const size_t NUM_VILLAGERS_START = 5;
 
 // Size of the map in grid elements
 const size_t MAP_SIZE = 30;
@@ -29,52 +29,52 @@ const size_t MAP_SIZE = 30;
 const size_t ELEMENT_SIZE = 10;
 
 // Max Starting HP for Soldier
-const int64_t SOLDIER_MAX_HP = 200;
+const int64_t SOLDIER_MAX_HP = 150;
 
 // Max Starting HP for Villager
-const int64_t VILLAGER_MAX_HP = 80;
+const int64_t VILLAGER_MAX_HP = 50;
 
 // HP of an unbuilt factory
-const int64_t FACTORY_BASE_HP = 1;
+const int64_t FACTORY_BASE_HP = 50;
 
 // Max Starting HP for Factory
-const int64_t FACTORY_MAX_HP = 3000;
+const int64_t FACTORY_MAX_HP = 4000;
 
 // Total construction effort needed per factory
-const int64_t FACTORY_CONSTRUCTION_TOTAL = 300;
+const int64_t FACTORY_CONSTRUCTION_TOTAL = 1000;
 
 // Frequency with which a factory produces villagers
-const int64_t FACTORY_VILLAGER_FREQUENCY = 10;
+const int64_t FACTORY_VILLAGER_FREQUENCY = 4;
 
 // Frequency with which a factory produces soldiers
-const int64_t FACTORY_SOLDIER_FREQUENCY = 15;
+const int64_t FACTORY_SOLDIER_FREQUENCY = 6;
 
 // Speed of a soldier
-const int64_t SOLDIER_SPEED = 5;
+const int64_t SOLDIER_SPEED = 6;
 
 // Speed of a villager
-const int64_t VILLAGER_SPEED = 3;
+const int64_t VILLAGER_SPEED = 5;
 
 // Attack range of a soldier
-const int64_t SOLDIER_ATTACK_RANGE = 5;
+const int64_t SOLDIER_ATTACK_RANGE = 9;
 
 // Attack range of a villager
-const int64_t VILLAGER_ATTACK_RANGE = 5;
+const int64_t VILLAGER_ATTACK_RANGE = 9;
 
 // Attack damage of a soldier
-const int64_t SOLDIER_ATTACK_DAMAGE = 10;
+const int64_t SOLDIER_ATTACK_DAMAGE = 15;
 
 // Attack damage of a villager
-const int64_t VILLAGER_ATTACK_DAMAGE = 5;
+const int64_t VILLAGER_ATTACK_DAMAGE = 9;
 
 // Build effort in one turn of the villager
-const int64_t VILLAGER_BUILD_EFFORT = 3;
+const int64_t VILLAGER_BUILD_EFFORT = 4;
 
 // Build range of the villager
-const int64_t VILLAGER_BUILD_RANGE = 5;
+const int64_t VILLAGER_BUILD_RANGE = 9;
 
 // Mine range of the villager
-const int64_t VILLAGER_MINE_RANGE = 5;
+const int64_t VILLAGER_MINE_RANGE = 9;
 
 // Player 1 start position
 // Player 2 is the same position flipped
